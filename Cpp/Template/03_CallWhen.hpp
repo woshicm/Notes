2d518c012e0fb24d493c873c771013d2d35a4297
woshicm <35488890+woshicm@users.noreply.github.com>
2024-02-22 13:56:22 +0800

    Update and rename 03_IsSameType to 03_CallWhen.hpp

---
+++ Cpp/Template/03_CallWhen.hpp
@@ -0,0 +1,32 @@
+/*
+ * @Date: 2024-02-22
+ * @LastEditTime: 2024-02-22
+ * @Description: 根据对象类型做决策
+ * 
+ * Copyright (c) 2024 by Chenmeng, All Rights Reserved. 
+ */
+
+	template<class T1, class T2>
+	struct is_sameT : std::false_type {};
+
+	template<class T>
+	struct is_sameT<T, T> : std::true_type {};
+
+	template<class T1, class T2>
+	using is_same = is_sameT<T1, T2>;
+
+	void testImpl(std::true_type)
+	{
+		std::cout << "True Type called";
+	}
+
+	void testImpl(std::false_type)
+	{
+		std::cout << "False Type called";
+	}
+
+	template<class T>
+	void test(T)
+	{
+		testImpl(is_same<bool, T>{});
+	}
