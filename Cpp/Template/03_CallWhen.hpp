27cb5c87887a1034bbf205f6b32e48976d21acd1
woshicm <35488890+woshicm@users.noreply.github.com>
2024-02-22 14:53:01 +0800

    Update 03_CallWhen.hpp

--- Cpp/Template/03_CallWhen.hpp
+++ Cpp/Template/03_CallWhen.hpp
@@ -7,27 +7,27 @@
  * Copyright (c) 2024 by Chenmeng, All Rights Reserved. 
  */
 
-	template<class T1, class T2>
-	struct is_sameT : std::false_type {};
+template<class T1, class T2>
+struct is_sameT : std::false_type {};
 
-	template<class T>
-	struct is_sameT<T, T> : std::true_type {};
+template<class T>
+struct is_sameT<T, T> : std::true_type {};
 
-	template<class T1, class T2>
-	using is_same = is_sameT<T1, T2>;
+template<class T1, class T2>
+using is_same = is_sameT<T1, T2>;
 
-	void testImpl(std::true_type)
-	{
-		std::cout << "True Type called";
-	}
+void testImpl(std::true_type)
+{
+	std::cout << "True Type called";
+}
 
-	void testImpl(std::false_type)
-	{
-		std::cout << "False Type called";
-	}
+void testImpl(std::false_type)
+{
+	std::cout << "False Type called";
+}
 
-	template<class T>
-	void test(T)
-	{
-		testImpl(is_same<bool, T>{});
-	}
+template<class T>
+void test(T)
+{
+	testImpl(is_same<bool, T>{});
+}
