b90e809c8182c4f26a7971a7cc34129cb0d684f6
woshicm <35488890+woshicm@users.noreply.github.com>
2024-03-26 15:30:37 +0800

    Update cm_clibs.hpp
    删除了使用Check_Ret标识返回值的内容，该需求由额外的#define语句实现

--- Cpp/Libs/cm_clibs.hpp
+++ Cpp/Libs/cm_clibs.hpp
@@ -1,10 +1,7 @@
 /*
- * @Author: woshicm 35488890+woshicm@users.noreply.github.com
  * @Date: 2024-03-18 21:09:24
- * @LastEditors: Please set LastEditors
  * @LastEditTime: 2024-03-26 14:56
- * @FilePath: \cm_clibs\main.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 提前返回语句的精简，并能指定输出字符串和返回值
  */
 #include <iostream>
 #include <fmt/core.h>
@@ -29,16 +26,8 @@ using namespace std::literals;
 
 namespace cmc
 {
-    struct check_ret_p{};
-    template<typename T, T val>
-    struct check_ret: public check_ret_p{
-        static constexpr T value = val;
-    };
-
-
     struct no_out_t{};
     inline constexpr no_out_t no_out{};
-
 
     template <typename T, typename... Ts>
     struct is_convertible_any {
@@ -47,44 +36,6 @@ namespace cmc
 
     template <typename T, typename... Ts>
     inline constexpr bool is_convertible_any_v = is_convertible_any<T, Ts...>::value;
-
-    struct Check_Ret{
-        template<typename T, typename... Args>
-        static constexpr int get_type_index()
-        {
-            int ret = -1, cnt = 0;
-            ((std::is_convertible_v<Args, T> ? ret = cnt : ++cnt), ...);
-            return ret;
-        }
-
-        template<typename... Args>
-        static constexpr int getIndex(Args... args)
-        {
-        return index<check_ret_p, Args...>;
-        }
-
-        template<typename... Args>
-        static constexpr int index = get_type_index<check_ret_p, Args...>();
-
-        template<size_t I, size_t N, typename T, typename... Args>
-        static auto _getValue(T t, Args... args)
-        {
-            if constexpr (I == N || N > sizeof...(Args))
-                return t;
-            else
-                return _getValue<I+1, N>(args...);
-        }
-
-        template<typename... Args, typename=std::enable_if_t<index<Args...> != -1>>
-        static constexpr auto getValue(Args... args)
-        {
-            return _getValue<0, index<Args...>>(args...).value;
-        }
-
-        template<typename... Args>
-        static constexpr void getValue(...)
-        {}
-    };
 
     struct Check_Print {
         template<typename... Args>
@@ -120,7 +71,6 @@ namespace cmc
     if (!(x)){ \
         __CHECK_PRINT_LINE(x, __VA_ARGS__) \
         if constexpr(fmt::basic_string_view(__FUNCSIG__).starts_with("void ")) return void(0);\
-    /* else if constexpr(Check_Ret::getIndex(__VA_ARGS__) != -1) return Check_Ret::getValue(__VA_ARGS__) false;*/\
         else return {};\
     }
 
@@ -134,7 +84,6 @@ namespace cmc
     if (!(x)){ \
         __CHECK_PRINT_LINE(x, __VA_ARGS__) \
         if constexpr(fmt::basic_string_view(__FUNCSIG__).starts_with("void ")) return void(0);\
-    /* else if constexpr(Check_Ret::getIndex(__VA_ARGS__) != -1) return Check_Ret::getValue(__VA_ARGS__) false;*/\
         else return {};\
     }
 
